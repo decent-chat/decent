47422dcd465a5036805e5ccaf294f5fcfdd586fc
Florrie <towerofnix@gmail.com>
2017-11-29 19:27:16 -0400

    Use HTTP API where it's useful
    Also added package.json.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const Datastore = require('nedb-promise')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
@@ -14,9 +15,40 @@ async function main() {
   await Promise.all(Object.values(db).map(d => d.loadDatabase()))
 
   app.use(express.static('site'))
+  app.use(bodyParser.json())
 
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/site/index.html')
+  })
+
+  app.post('/api/send-message', async (request, response) => {
+    const { text, signature, userID } = request.body
+
+    if (!text || !userID) {
+      return
+    }
+
+    const message = await db.messages.insert({
+      text: request.body.text,
+      signature: request.body.signature,
+      author: request.body.userID,
+      date: Date.now()
+    })
+
+    io.emit('received chat message', {message})
+
+    response.end('sent')
+  })
+
+
+  app.post('/api/release-public-key', async (request, response) => {
+    const { key, userID } = request.body
+
+    if (!key || !userID) {
+      return
+    }
+
+    io.emit('released public key', {key, userID})
   })
 
   io.on('connection', socket => {
@@ -24,48 +56,6 @@ async function main() {
 
     socket.on('disconnect', () => {
       console.log('a user disconnected')
-    })
-
-    socket.on('send chat message', async msg => {
-      if (typeof msg !== 'object') {
-        return
-      }
-
-      if ('text' in msg === false) {
-        return
-      }
-
-      if ('userID' in msg === false) {
-        return
-      }
-
-      const message = await db.messages.insert({
-        text: msg.text,
-        signature: msg.signature,
-        author: msg.userID,
-        date: Date.now()
-      })
-
-      io.emit('received chat message', {message})
-    })
-
-    socket.on('release public key', msg => {
-      if (typeof msg !== 'object') {
-        return
-      }
-
-      if ('key' in msg === false) {
-        return
-      }
-
-      if ('userID' in msg === false) {
-        return
-      }
-
-      io.emit('released public key', {
-        key: msg.key,
-        userID: msg.userID
-      })
     })
   })
 
