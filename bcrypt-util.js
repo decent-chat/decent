b81d069e59047393e613bce06538411c68f8b6be
Florrie <towerofnix@gmail.com>
2017-12-01 15:42:59 -0400

    Move API into separate file from index.js

---
+++ bcrypt-util.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt-nodejs')
+
+module.exports.genSalt = (rounds = 10) => new Promise((resolve, reject) => {
+  bcrypt.genSalt(rounds, (err, salt) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(salt)
+    }
+  })
+})
+
+module.exports.hash = (str, salt) => new Promise((resolve, reject) => {
+  bcrypt.hash(str, salt, null, (err, hash) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(hash)
+    }
+  })
+})
+
+module.exports.compare = (data, encrypted) => new Promise((resolve, reject) => {
+  bcrypt.compare(data, encrypted, (err, result) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(result)
+    }
+  })
+})
+
