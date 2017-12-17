8ed2e0cf5672ad64ff44682c41b0ed300a17dfc1
Florrie <towerofnix@gmail.com>
2017-12-16 21:30:28 -0400

    Make login button work, begin using oof for real

--- site/index.js
+++ site/index.js
@@ -1,12 +1,66 @@
 'use strict'
 
-const items = ['a', 'b', 'c']
+// Utility functions:
 
-const activeItemIndex = new Value(0)
-const activeItem = new Reference(items, activeItemIndex)
+function post(path, dataObj, serverHostname = activeServerHostname) {
+  const serverURL = '//' + Changeable.valueOf(serverHostname)
+  return fetch(serverURL + '/api/' + path, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dataObj)
+  }).then(res => res.json())
+}
 
-activeItem.onChange(newActiveItem => {
-  console.log(newActiveItem)
+function get(path, dataObj, serverHostname = activeServerHostname) {
+  const esc = encodeURIComponent
+  const queryString = Object.keys(query).length > 0
+    ? '?' + Object.keys(query)
+      .map(k => esc(k) + '=' + esc(query[k]))
+      .join('&')
+    : ''
+
+  // TODO: This should probably be a computed lol
+  const serverURL = '//' + Changeable.valueOf(serverHostname)
+  return fetch(serverURL + '/api/' + path + queryString)
+    .then(res => res.json())
+}
+
+// The actual client code:
+
+const serverDict = new Dictionary()
+const activeServerHostname = new Value()
+const currentServer = new Reference(serverDict, activeServerHostname)
+const sessionID = new Reference(currentServer, 'sessionID')
+
+sessionID.onChange(id => {
+  console.log('New session ID:', id)
 })
 
-activeItemIndex.set(1)
+function addServer(serverHostname) {
+  serverDict[serverHostname] = new Dictionary({
+    sessionID: null
+  })
+
+  activeServerHostname.set(serverHostname)
+}
+
+addServer('localhost:2999')
+
+document.getElementById('login').addEventListener('click', async () => {
+  if (currentServer.value === null) {
+    alert('Excuse me, you aren\'t on a server???')
+  }
+
+  const username = prompt('Username?')
+  const password = prompt('Password? (Insert speel about DON\'T SEND SENSITIVE PASSWORDS OVER HTTP here)')
+
+  if (username && password) {
+    const result = await post('login', {username, password})
+
+    if (result.success === true) {
+      currentServer.value.sessionID = result.sessionID
+    }
+  }
+})
