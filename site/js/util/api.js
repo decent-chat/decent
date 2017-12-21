68f355bfa6994e3e63d0eebbb4dce8f895396559
alex <16batesa@gmail.com>
2017-12-21 18:14:11 +0000

    Throw if api.postRaw body not provided
    Before, it would assume no body, which was not intended

--- site/js/util/api.js
+++ site/js/util/api.js
@@ -49,7 +49,7 @@ module.exports = {
     })
   },
 
-  postRaw(state, path, body = '') {
+  postRaw(state, path, body) {
     if (!body) return Promise.reject(new Error('Body not provided'))
 
     return fetchHelper(state, path, {
