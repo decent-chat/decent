294d94185fc2c126abb11658c443d78ed016e2c3
alex <16batesa@gmail.com>
2018-03-03 15:04:03 +0000

    Delete packages/client/src/
    Rest in pizza.

--- packages/client/src/util/index.js
+++
@@ -1,8 +0,0 @@
-const WS = require('./ws')
-const api = require('./api')
-const Modal = require('./modal')
-const storage = require('./storage')
-const svg = require('./svg')
-const mrk = require('./mrk')
-
-module.exports = {WS, api, Modal, storage, svg, mrk}
