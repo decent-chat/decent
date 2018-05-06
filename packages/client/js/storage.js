a0c5e30eefdf77ab8fa6758f4d228800884ed4cc
alex <16batesa@gmail.com>
2018-05-06 18:05:30 +0100

    Remove logs
    Before I forget

--- packages/client/js/storage.js
+++ packages/client/js/storage.js
@@ -3,15 +3,12 @@ const storage = /*window.sessionStorage ||*/ window.localStorage
 function save(key, value) {
   if (value === undefined) storage.removeItem(key)
   else storage.setItem(key, JSON.stringify(value))
-
-  console.log(`Storage: set ${key} =`, value)
 }
 
 function load(key, defaultValue) {
   const str = storage.getItem(key)
   const value = str ? JSON.parse(str) : defaultValue
 
-  console.log(`Storage: loaded ${key} =`, value)
   return value
 }
 
